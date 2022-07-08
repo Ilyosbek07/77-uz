1129dd6ad17cba5be8ba5006f06e1f6a3fe52a6f
Amon Olimov <i.jalolov@uicgroup.org>
2022-07-08 09:34:09 +0500

    feat: base, common apps & locale

--- django_template/settings/base.py
+++ django_template/settings/base.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'django_template.urls'
@@ -65,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'src.base.context_processors.project_languages'
             ],
         },
     },
@@ -89,10 +91,14 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 # END AUTH USER
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
+
+# LANGUAGES = (
+#     ('ru', 'Russian'),
+#     ('en', 'English'),
+# )
 
 TIME_ZONE = 'UTC'
 
